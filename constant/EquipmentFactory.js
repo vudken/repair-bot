0a9140c54e0ec4cbfd3bdd9196c30dc9e04fd88b
Ken Vud <vudken@gmail.com>
2023-08-08 14:02:58 +0300

    EQUIPMENT FACTORY

---
+++ constant/EquipmentFactory.js
@@ -0,0 +1,33 @@
+const EQUIPMENT_CATEGORY = require('../constant/EquipmentCategoryEnum');
+const conn = require('../db/conn');
+
+class EquipmentFactory {
+    create = async (category) => {
+        if (!category) return `Unable to get equipment. Please specify a category and tryagain!`;
+
+        let equipment;
+        switch (category) {
+            case EQUIPMENT_CATEGORY.CONTROLLER:
+                equipment = await conn.getAllControllers();
+                break;
+            case EQUIPMENT_CATEGORY.COUNTER:
+                equipment = await conn.getAllCounters();
+                break;
+            case EQUIPMENT_CATEGORY.PUMP:
+                equipment = await conn.getAllPumps();
+                break;
+            case EQUIPMENT_CATEGORY.REGULATOR:
+                equipment = await conn.getAllRegulators();
+                break;
+            case EQUIPMENT_CATEGORY.SENSOR:
+                equipment = await conn.getAllSensors();
+                break;
+            default:
+                break;
+        }
+
+        return equipment;
+    };
+}
+
+module.exports = EquipmentFactory;
---
+++ constant/user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('dotenv').config();
+
+const env = process.env;
+const user = {
+    0: { name: 'Test user', chatId: parseInt(env.TEST_USER_ID) },
+    1: { name: 'Boss', chatId: parseInt(env.BOSS_USER_ID) },
+
+    isKeyValid: function (key) {
+        return this.hasOwnProperty(key);
+    },
+
+    isValid: function (id) {
+        return this.getAllChatIds().some(chatId => chatId === id);
+    },
+
+    isBoss: function (id) {
+        return this[1].chatId == id;
+    },
+
+    getAllChatIds: function () {
+        return Object.values(this)
+            .filter(val => typeof val != 'function')
+            .map(val => val.chatId);
+    },
+};
+
+module.exports = user;
